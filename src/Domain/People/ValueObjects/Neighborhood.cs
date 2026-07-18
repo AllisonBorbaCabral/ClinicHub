e6ca2d29ebbe59d23aa726fe8d6cabcffa54872d
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-18 17:12:00 -0300

    ValueObject de bairro

---
+++ src/Domain/People/ValueObjects/Neighborhood.cs
@@ -0,0 +1,34 @@
+using DemoMVC.Shared.Results;
+using System.Text.RegularExpressions;
+
+namespace DemoMVC.Domain.People.ValueObjects;
+
+public sealed class Neighborhood
+{
+    public string Value { get; private set; } = null!;
+    private Neighborhood() { }
+    private Neighborhood(string value)
+    {
+        Value = value;
+    }
+    public Result<Neighborhood> Create(string value)
+    {
+        var neighborhood = value.Trim();
+
+        neighborhood = Regex.Replace(neighborhood, @"\s+", " ");
+
+        if (string.IsNullOrWhiteSpace(neighborhood))
+            return Result<Neighborhood>.Fail("O campo bairro não pode ser vazio.");
+
+        if (neighborhood.Length < 3)
+            return Result<Neighborhood>.Fail("O campo bairro deve ter no mínimo 3 caracteres.");
+
+        if (neighborhood.Length > 200)
+            return Result<Neighborhood>.Fail("O campo bairro deve ter no máximo 200 caracteres.");
+
+        if (neighborhood.Distinct().Count() == 1)
+            return Result<Neighborhood>.Fail("O campo bairro deve ter mais de um caracter.");
+
+        return Result<Neighborhood>.Ok(new Neighborhood(neighborhood));
+    }
+}
