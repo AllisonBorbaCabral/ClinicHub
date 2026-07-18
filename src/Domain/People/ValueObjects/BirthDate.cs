457b346c971a0b4f03212d5fa6c8d5f42c17f258
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-18 16:51:58 -0300

    ValueObject da data de nascimento

---
+++ src/Domain/People/ValueObjects/BirthDate.cs
@@ -0,0 +1,23 @@
+using DemoMVC.Shared.Results;
+
+namespace DemoMVC.Domain.People.ValueObjects;
+
+public sealed class BirthDate
+{
+    public DateOnly Value { get; private set; }
+    private BirthDate() { }
+    private BirthDate(DateOnly value)
+    {
+        Value = value;
+    }
+    public Result<BirthDate> Create(DateOnly value)
+    {
+        DateOnly min = new DateOnly(1900, 1, 1);
+        DateOnly max = DateOnly.FromDateTime(DateTime.Today);
+
+        if (value < min || value > max)
+            return Result<BirthDate>.Fail("Data de nascimento inválida.");
+
+        return Result<BirthDate>.Ok(new BirthDate(value));
+    }
+}
