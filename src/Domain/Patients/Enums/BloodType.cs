87c1e181be967eb0f7cf33f2bb59a33d4f017981
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-19 21:00:32 -0300

    Enum de tipagem sanguinea

---
+++ src/Domain/Patients/Enums/BloodType.cs
@@ -0,0 +1,13 @@
+namespace DemoMVC.Domain.Patients.Enums;
+
+public enum BloodType
+{
+    APos = 1,
+    ANeg = 2,
+    BPos = 3,
+    BNeg = 4,
+    ABPos = 5,
+    ABNeg = 6,
+    OPos = 7,
+    ONeg = 8
+}
