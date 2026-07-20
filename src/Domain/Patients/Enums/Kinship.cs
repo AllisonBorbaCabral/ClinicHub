d04f85a199930903a23ab788a736f9f31a4ef23b
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-19 21:00:59 -0300

    Enum de parentesco

---
+++ src/Domain/Patients/Enums/Kinship.cs
@@ -0,0 +1,12 @@
+namespace DemoMVC.Domain.Patients.Enums;
+
+public enum Kinship
+{
+    Spouse = 1,
+    Child = 2,
+    Parent = 3,
+    Sibling = 4,
+    Grandparent = 5,
+    Grandchild = 6,
+    Other = 7
+}
