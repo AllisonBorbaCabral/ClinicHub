7574e0603b2f7f4b5fbcbdb4a9b86978ce034f9b
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-19 23:49:46 -0300

    Ajuste no DbContext para suporte do mapping

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,47 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using DemoMVC.Domain.Patients.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using DemoMVC.Domain.People.Entities;
 
 namespace DemoMVC.Infrastructure.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext(options)
 {
-    public DbSet<Patient> Patients => Set<Patient>();
+    public DbSet<Person> Persons => Set<Person>();
+    // public DbSet<Patient> Patients => Set<Patient>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
         base.OnModelCreating(builder);
-
-        builder.Entity<Patient>(e =>
-        {
-            e.ToTable("patients");
-
-            e.HasKey(e => e.Id);
-
-            e.Property(e => e.Id)
-             .HasColumnName("id");
-
-            e.Property(e => e.Name)
-                .HasColumnName("name")
-                .HasMaxLength(200)
-                .IsRequired();
-
-            e.Property(e => e.BirthDate)
-                .HasColumnName("birthdate")
-                .HasColumnType("date")
-                .IsRequired();
-
-            e.Property(e => e.IsActive)
-                .HasColumnName("is_active")
-                .IsRequired();
-
-            e.Property(e => e.CreatedAt)
-                .HasColumnName("created_at")
-                .IsRequired();
-
-            e.Property(e => e.UpdatedAt)
-                .HasColumnName("updated_at");
-        });
     }
 }
 
