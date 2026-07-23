1d99f24008847195bd852b0f6eb13a07080b9fe8
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-23 11:08:54 -0300

    Implementado geração de sequence ao realizar o migrate do projeto

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using DemoMVC.Domain.People.Entities;
 using DemoMVC.Domain.Patients.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using DemoMVC.Domain.People.Entities;
 
 namespace DemoMVC.Infrastructure.Data;
 
@@ -9,13 +9,26 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     : IdentityDbContext(options)
 {
     public DbSet<Person> Persons => Set<Person>();
-    // public DbSet<Patient> Patients => Set<Patient>();
+    public DbSet<Patient> Patients => Set<Patient>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        base.OnModelCreating(builder);
+
         builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
-        base.OnModelCreating(builder);
+        ConfigureSequences(builder);
+    }
+    private static void ConfigureSequences(ModelBuilder builder)
+    {
+        builder.HasSequence<int>("medical_record_sequence")
+            .StartsAt(1)
+            .IncrementsBy(1);
+
+        builder.HasSequence<int>("appointment_sequence")
+        .StartsAt(1)
+        .IncrementsBy(1);
+
     }
 }
 
