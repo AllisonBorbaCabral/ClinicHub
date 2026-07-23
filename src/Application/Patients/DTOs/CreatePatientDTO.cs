2401873e45ce9c18f092f98d44784537061cf8b8
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-23 11:20:09 -0300

    Ajuste no CreateDTO de patient

--- src/Application/Patients/DTOs/CreatePatientDTO.cs
+++ src/Application/Patients/DTOs/CreatePatientDTO.cs
@@ -1,7 +1,26 @@
 namespace DemoMVC.Application.Patients.DTOs;
 
 public sealed record CreatePatientDTO(
+    Guid? PersonId,
     string Name,
     DateOnly BirthDate,
+    string Cpf,
+    string? Street,
+    string? StreetNumber,
+    string? Neighborhood,
+    string? AddressLine,
+    string? PostalCode,
+    string? PhoneNumber,
+    string? Email,
+    string? InsuranceId,
+    DateOnly? InsuranceDueDate,
+    int BloodType,
+    string? ResponsibleName,
+    string? ReponsibleCpf,
+    int? ResponsibleKinship,
+    string? ResponsibleNumber,
+    string? EmergencyContactName,
+    string? EmergencyContactNumber,
+    int? EmergencyContactKinship,
     bool IsActive
 );
