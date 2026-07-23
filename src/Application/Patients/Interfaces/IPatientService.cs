3b71d887c3dd7f91d91625a8f020d2a418bae8c4
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-23 11:19:42 -0300

    Ajuste na interface do service de patient

--- src/Application/Patients/Interfaces/IPatientService.cs
+++ src/Application/Patients/Interfaces/IPatientService.cs
@@ -1,12 +1,13 @@
 using DemoMVC.Application.Patients.DTOs;
+using DemoMVC.Shared.Results;
 
 namespace DemoMVC.Application.Patients.Interfaces;
 
 public interface IPatientService
 {
-    Task<IReadOnlyList<GetPatientDTO>> GetAllAsync();
-    Task<GetPatientDTO?> GetByIdAsync(Guid id);
-    Task<GetPatientDTO?> GetByNameAsync(string name);
+    Task<Result<IReadOnlyList<ListPatientDTO>>> GetAllAsync();
+    Task<Result<DetailPatientDTO>> GetForUpdateAsync(Guid id);
+    // Task<Result<GetPatientDTO?>> GetByNameAsync(string name);
     // Task<CustomerViewModel?> GetByDocument(string document);
-    Task<GetPatientDTO?> Create(CreatePatientDTO request);
+    Task<Result<Guid?>> Create(CreatePatientDTO request);
 }
