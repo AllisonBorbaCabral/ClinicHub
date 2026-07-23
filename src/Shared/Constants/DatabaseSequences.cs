727b1a2df8d2b1c389b5c28b766aed483c15c7c6
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-23 11:23:56 -0300

    Criada classe auxiliar com a const das sequences atuais e futuras

---
+++ src/Shared/Constants/DatabaseSequences.cs
@@ -0,0 +1,10 @@
+namespace DemoMVC.Shared.Constants;
+
+public static class DatabaseSequences
+{
+    public const string MedicalRecord =
+        "medical_record_sequence";
+
+    // public const string Appointment =
+    //     "appointment_sequence";
+}
