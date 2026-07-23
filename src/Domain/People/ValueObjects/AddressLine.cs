c56b13d7688634b22547595e927d95bad5b6baae
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-23 10:52:52 -0300

    Ajuste de validação no ValueObject

--- src/Domain/People/ValueObjects/AddressLine.cs
+++ src/Domain/People/ValueObjects/AddressLine.cs
@@ -11,21 +11,24 @@ public sealed class AddressLine
     {
         Value = value;
     }
-    public static Result<AddressLine> Create(string value)
+    public static Result<AddressLine?> Create(string? value)
     {
+        if (string.IsNullOrEmpty(value))
+            return Result<AddressLine?>.Ok(null);
+
         var addressLine = value.Trim();
 
         addressLine = Regex.Replace(addressLine, @"\s+", " ");
 
         if (string.IsNullOrWhiteSpace(addressLine))
-            return Result<AddressLine>.Fail("Complemento não pode ser nulo ou vazio.");
+            return Result<AddressLine?>.Fail("Complemento não pode ser nulo ou vazio.");
 
         if (addressLine.Length > 100)
-            return Result<AddressLine>.Fail("Complemento deve ter no máximo 100 caracteres.");
+            return Result<AddressLine?>.Fail("Complemento deve ter no máximo 100 caracteres.");
 
         if (addressLine.Distinct().Count() == 1)
-            return Result<AddressLine>.Fail("Complemento deve ter mais de um caracter.");
+            return Result<AddressLine?>.Fail("Complemento deve ter mais de um caracter.");
 
-        return Result<AddressLine>.Ok(new AddressLine(addressLine));
+        return Result<AddressLine?>.Ok(new AddressLine(addressLine));
     }
 }
