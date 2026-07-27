8c79e3aa06dbcb8586bbc0efbec364c85cafa1b5
Allison Borba Cabral <allisonborba@hotmail.com>
2026-07-26 22:02:12 -0300

    refactor: refactor Result Pattern

--- src/Shared/Results/Result.cs
+++ src/Shared/Results/Result.cs
@@ -2,42 +2,38 @@ namespace DemoMVC.Shared.Results;
 
 public class Result
 {
-    public bool Success { get; protected set; }
-    public List<string> Errors { get; protected set; } = new();
-
+    public bool Success { get; }
+    public bool IsFailure => !Success;
+    public IReadOnlyList<string> Errors { get; }
+    protected Result(bool success, IEnumerable<string>? errors = null)
+    {
+        Success = success;
+        Errors = errors?.ToList().AsReadOnly() ?? new List<string>().AsReadOnly();
+    }
     public static Result Ok()
-        => new() { Success = true };
-
+        => new(true);
     public static Result Fail(params string[] errors)
-        => new()
-        {
-            Success = false,
-            Errors = errors.ToList()
-        };
-
+        => new(false, errors.Length > 0 ? errors : new[] { "Ocorreu um erro inesperado." });
     public static Result Fail(IEnumerable<string> errors)
-        => new()
-        {
-            Success = false,
-            Errors = errors.ToList()
-        };
+        => new(false, errors);
 }
-
 public class Result<T> : Result
 {
-    public T? Data { get; private set; }
-
+    public T Data { get; }
+    private Result(T data) : base(true)
+    {
+        Data = data;
+    }
+    private Result(IEnumerable<string> errors) : base(false, errors)
+    {
+        Data = default!;
+    }
     public static Result<T> Ok(T data)
-        => new()
-        {
-            Success = true,
-            Data = data
-        };
-
+        => data is not null
+            ? new Result<T>(data)
+            : throw new ArgumentException(nameof(data), "É necessário um valor válido.");
     public new static Result<T> Fail(params string[] errors)
-        => new()
-        {
-            Success = false,
-            Errors = errors.ToList()
-        };
+        => new(errors);
+    public new static Result<T> Fail(IEnumerable<string> errors)
+        => new(errors);
 }
